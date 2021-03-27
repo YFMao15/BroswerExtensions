d10f9cb9c582c62d7097d42a7408dd08fbddaaba
Yifeng Mao <34615806+YFMao15@users.noreply.github.com>
2021-03-27 03:06:04 -0400

    new updates on ads skipping

--- SkipYoutubeAds/script.js
+++ SkipYoutubeAds/script.js
@@ -1,23 +1,19 @@
-// Skip the ads in the front
-if (document.getElementsByClassName("ytp-ad-skip-button").length > 0) {
-    document.getElementsByClassName("ytp-ad-skip-button")[0].click();
-    console.log("front ads jumped");
-}
-
-// Skip the ads in the middle
-var video = document.getElementsByClassName('html5-video-player')[0];
-var config = {attributes: true};
+var ad_component = document.getElementsByClassName('video-ads')[0];
+var config = {childList: true};
 
 var callback = function(mutations, observer) {
     for(var mutation of mutations) {
-        if (mutation.type === 'attributes') {
+        if (mutation.type === 'childList') {
             if (document.getElementsByClassName("ytp-ad-skip-button").length > 0) {
                 document.getElementsByClassName("ytp-ad-skip-button")[0].click();
-                console.log("middle ads jumped");
+                console.log("long-time ads skipped");
+            } else if (document.getElementsByClassName("ytp-ad-overlay-close-button").length > 0) {
+                document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+                console.log("subtitle ads skipped")
             }
         }
     }
 };
 
 var observer = new MutationObserver(callback);
-observer.observe(video, config);
+observer.observe(ad_component, config);
